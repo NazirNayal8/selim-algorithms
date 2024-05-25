dbed084c232433983f1702e12a1062d1b6923edd
Nazir Nayal <nnayal17@ku.edu.tr>
2024-05-25 11:02:13 +0300

    add dijkstra

--- graph/search_algorithms.cpp
+++ graph/search_algorithms.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 
-const int n = 9;
-const int m = 8;
+const int n = 10;
+const int m = 13;
 
 vector<int> graph[n + 1];
 vector<pair<int,int>> graph_weighted[n + 1];
@@ -87,6 +87,8 @@ int tree_distance(int start_node, int target_node) {
     vector<int> dist(n + 1, -1);
     vector<int> parent(n + 1, -1);
 
+    // parent[i] = j means that j is the parent of i
+
     q.push(start_node);
     visited[start_node] = true;
     dist[start_node] = 0;
@@ -105,6 +107,48 @@ int tree_distance(int start_node, int target_node) {
                 q.push(neighbour);
                 visited[neighbour] = true;
                 dist[neighbour] = dist[node] + weight;
+                parent[neighbour] = node;
+            }
+        }
+    }
+    
+    int i = target_node;
+    while (parent[i]!=-1) {
+        cout<<parent[i] << endl;
+        i=parent[i];
+    }
+
+    return dist[target_node];
+}
+
+
+int dijkstra_distance(int start_node, int target_node) {
+
+    vector<bool> visited(n + 1, false);
+    priority_queue<pair<int,int>> q;
+    vector<int> dist(n + 1, -1);
+    
+    // parent[i] = j means that j is the parent of i
+
+    q.push(make_pair(0, start_node));
+    visited[start_node] = true;
+    dist[start_node] = 0;
+
+    while (!q.empty()) {
+        pair<int,int> pair = q.top();
+        int node = pair.second;
+        dist[node] = -pair.first;
+        q.pop();
+
+        // cout << node << " is visited" << endl;
+
+        for (int i = 0; i < graph_weighted[node].size(); i++) {
+            int neighbour = graph_weighted[node][i].first;
+            int weight = graph_weighted[node][i].second;
+
+            if (dist[neighbour] == -1 || dist[neighbour] > dist[node] + weight) {
+                dist[neighbour] = dist[node] + weight;
+                q.push(make_pair(-dist[neighbour], neighbour));
             }
         }
     }
@@ -113,7 +157,8 @@ int tree_distance(int start_node, int target_node) {
 }
 
 
-int main() {
+
+void initialize_tree() {
 
     graph_weighted[1].push_back(make_pair(2, 4));
     graph_weighted[2].push_back(make_pair(1, 4));
@@ -139,6 +184,57 @@ int main() {
     graph_weighted[7].push_back(make_pair(9, 3));
     graph_weighted[9].push_back(make_pair(7, 3));
 
+}
+
+
+void initialize_graph() {
+
+    graph_weighted[1].push_back(make_pair(2, 4));
+    graph_weighted[2].push_back(make_pair(1, 4));
+
+    graph_weighted[2].push_back(make_pair(3, 1));
+    graph_weighted[3].push_back(make_pair(2, 1));
+
+    graph_weighted[2].push_back(make_pair(6, 2));
+    graph_weighted[6].push_back(make_pair(2, 2));
+
+    graph_weighted[2].push_back(make_pair(7, 2));
+    graph_weighted[7].push_back(make_pair(2, 2));
+
+    graph_weighted[3].push_back(make_pair(4, 2));
+    graph_weighted[4].push_back(make_pair(3, 2));
+
+    graph_weighted[3].push_back(make_pair(5, 3));
+    graph_weighted[5].push_back(make_pair(3, 3));
+
+    graph_weighted[7].push_back(make_pair(8, 4));
+    graph_weighted[8].push_back(make_pair(7, 4));
+
+    graph_weighted[7].push_back(make_pair(9, 3));
+    graph_weighted[9].push_back(make_pair(7, 3));
+
+    graph_weighted[6].push_back(make_pair(10, 1));
+    graph_weighted[10].push_back(make_pair(6, 1));
+
+    graph_weighted[9].push_back(make_pair(10, 4));
+    graph_weighted[10].push_back(make_pair(9, 4));
+
+    graph_weighted[5].push_back(make_pair(8, 2));
+    graph_weighted[8].push_back(make_pair(5, 2));
+
+    graph_weighted[3].push_back(make_pair(7, 1));
+    graph_weighted[7].push_back(make_pair(3, 1));
+
+    graph_weighted[4].push_back(make_pair(5, 2));
+    graph_weighted[5].push_back(make_pair(4, 2));
+
+}
+
+
+int main() {
+
+   initialize_tree();
+
     // testing the code
     // cout << "DFS:" << endl;
     // dfs(4);
@@ -150,7 +246,7 @@ int main() {
     // bfs(4);
 
     int start_node = 8;
-    int target_node = 4;
-
+    int target_node = 5;
+    
     cout << "Distance between " << start_node << " and " << target_node << " is " << tree_distance(start_node, target_node) << endl;
 }
