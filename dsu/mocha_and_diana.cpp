d66cfb918202b6e41f2ec09cd5e60467d329d548
Nazir Nayal <nnayal17@ku.edu.tr>
2025-04-26 10:03:42 +0200

    add a dsu problem

--- dsu/mocha_and_diana.cpp
+++ dsu/mocha_and_diana.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <vector>
-
+#include "library.h"
 using namespace std;
-
 int main() {
-    
+    vector<pair<int,int> > q;
+    int t,y,l,n;
+    cin>>n;
+    cin>>t;
+    cin>>y;
+    DSU mocha(t);
+    DSU diana(y);
+    for(int i=1;i<=t;i++){
+        int k,w;
+        cin>>k;
+        cin>>w;
+        mocha.unite(k,w);
+    }
+    for(int i=1;i<=y;i++){
+        int k,w;
+        cin>>k;
+        cin>>w;
+        diana.unite(k,w);
+    }
+    for(int i=1;i<n;i++){
+        for(int x=i+1;x<=n;x++){
+            if(mocha.find(x)!=mocha.find(i) and diana.find(x)!=diana.find(i)){
+                q.push_back(make_pair(x,i));
+                mocha.unite(x,i);
+                l=l+1;
+            }
+        }
+    }
+    cout<<l<<endl;
+    for (int i=0;i<q.size();i++){
+        cout<<q[i].first<<" "<<q[i].second<<endl;
+    }
 }
--- library.h
+++ library.h
@@ -1,2 +1,62 @@
 #include <iostream>
 #include <vector>
+int find(DSU dsu, int x) {
+    while (dsu.leader[x] != x) {
+        dsu.leader[x] = dsu.leader[dsu.leader[x]];
+        x = dsu.leader[x];
+    }
+    return x;
+}
+
+struct DSU {
+
+    vector<int> leader;
+    vector<int> levels;
+    vector<int> sizes;
+    int leaders;
+
+    // constructor
+    DSU(int n) {
+        leader.resize(n + 1);
+        levels.resize(n + 1);
+        sizes.resize(n + 1);
+        for (int i = 1; i <= n ; i++) {
+            leader[i] = i;
+            levels[i] = 1;
+            sizes[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        while (leader[x] != x) {
+            leader[x] = leader[leader[x]];
+            x = leader[x];
+        }
+        return x;
+    }
+
+    void unite(int x, int y) {
+        int leader_x = find(x);
+        int leader_y = find(y);
+
+        if (leader_x == leader_y) {
+            return;
+        }
+
+        if (levels[leader_x] > levels[leader_y]) {
+            leader[leader_y] = leader_x;
+            sizes[leader_x] += sizes[leader_y];
+            leaders--;
+        } else if (levels[leader_x] < levels[leader_y]) {
+            leader[leader_x] = leader_y;
+            sizes[leader_y] += sizes[leader_x];
+            leaders--;
+        } else {
+            leader[leader_x] = leader_y;
+            levels[leader_y]++;
+            sizes[leader_y] += sizes[leader_x];
+            leaders--;
+        }
+    }
+
+};
